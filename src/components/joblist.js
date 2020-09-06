e5fb6d8bfbc1174bef30d84300ae8a941a7e5efc
Ishan Manandhar <ishan02016@gmail.com>
2020-09-06 20:05:17 +0545

    added styles

--- src/components/joblist.js
+++ src/components/joblist.js
@@ -4,38 +4,35 @@ import pin from '../assets/pin.svg';
 export function Jobslist({ jobs }) {
   return (
     <>
-      <div className="JoblistCard flexContainer">
-        <div className="flexConatiner">
-          <div className="JoblistCard_logo">
-            {jobs.company.logoUrl}
+      <div className="JoblistCard flexContainer fullFlex">
+        <div className="flexContainer JoblistCard_tip">
+          <div className="JoblistCard_logo flexConatiner">
             {!jobs.company.logoUrl ? (
-              <div>
-                <span className="nologo">no logo</span>
-              </div>
+              <span className="nologo">NA</span>
             ) : (
-              <img src={jobs.company.logoUrl} alt="logo" />
+              <img className="brandLogo" src={jobs.company.logoUrl} alt="logo" />
             )}
           </div>
           <div className="JoblistCard_title">
-            {jobs.title}
+            <p>{jobs.title}</p>
             <span>{jobs.company.name}</span>
           </div>
         </div>
-        <div className="flexConatiner JoblistCard_tags">
-          <ul>
+        <div className="flexContainer JoblistCard_tags">
+          <ul className="flexContainer">
             {jobs.tags.map((tags) => (
-              <li key={tags.id}>{tags.name}</li>
+              <li className="jobTag" key={tags.id}>{tags.name}</li>
             ))}
           </ul>
         </div>
-        <div className="flexConatiner JoblistCard_loc">
+        <div className="flexContainer JoblistCard_loc">
           <img src={pin} alt="pin" />
           <ul>
             {jobs.cities.map((cities) => (
               <li key={cities.id}>{cities.name}</li>
             ))}
           </ul>
-          <ul>
+          <ul className="remote">
             {jobs.remotes.map((remotes) => (
               <li key={remotes.id}>{remotes.type}</li>
             ))}
