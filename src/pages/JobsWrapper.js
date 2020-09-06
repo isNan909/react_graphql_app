2b889c347e887eb8aed8fab06660d8ed46a356c3
Ishan Manandhar <ishan02016@gmail.com>
2020-09-06 18:39:16 +0545

    added null conditionals

--- src/pages/JobsWrapper.js
+++ src/pages/JobsWrapper.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { Jobslist } from '../components/joblist';
 import GET_JOBLIST from '../graphql/get-joblist';
